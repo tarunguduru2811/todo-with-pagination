a31fd3036f5420e313027c63447a8c000d8f975e
Tarun2811 <tarunguduru123@gmail.com>
2026-04-07 17:39:52 +0530

    feat(observability): configure prometheus metrics and ServiceMonitor

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,26 @@ const todoRoutes = require("./routes/todoRoutes")
 const cors = require("cors");
 const { dbConnect } = require("./config/dbConnect");
 const dotenv = require("dotenv")
+const promBundle = require("express-prom-bundle");
+
 dotenv.config();
 
 app.use(cors({
     origin:"*"
 }));
 app.use(express.json())
+
+// Prometheus Metrics Middleware
+const metricsMiddleware = promBundle({
+    includeMethod: true, 
+    includePath: true, 
+    includeStatusCode: true, 
+    includeUp: true,
+    promClient: {
+        collectDefaultMetrics: {}
+    }
+});
+app.use(metricsMiddleware);
 dbConnect();
 
 const PORT = process.env.PORT;
